dc18318561bf274e5550f5d6647cc4dd0d249ef1
Follaabuelas <carlosdanielpulidom2@gmail.com>
2024-10-23 15:50:03 -0600

    Vista enemy

--- Assets/Scripts/EnemyRaycast.cs
+++ Assets/Scripts/EnemyRaycast.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class EnemyRaycast : MonoBehaviour
 {
-    RaycastHit[] hits;
+    public RaycastHit hit;
+    public float range;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 12; i++)
-        {
-            RaycastHit ray;
-
-        }
+        //for (int i = 0; i < 12; i++)
+        //{
+        //    RaycastHit ray = new RaycastHit();
+        //    ray.distance = range;
+        //    hits[i] = ray;
+        //}
     }
 
     // Update is called once per frame
@@ -24,17 +28,19 @@ public class EnemyRaycast : MonoBehaviour
 
     private void FixedUpdate()
     {
-        
-
-
-
-        //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),out hit, Mathf.Infinity))
-        //{
-        //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
-        //}
-        //else
-        //{
-        //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 50, Color.blue);
-        //}
+        for(int i = 1;i < 12; i++)
+        {
+            Vector3 angulo = new Vector3(transform.rotation.x, -90 + (i*5),transform.rotation.z);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit,range))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward + angulo) * range, Color.red);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward + angulo)* range, Color.blue);
+            }
+        }
+    
+       
     }
 }
