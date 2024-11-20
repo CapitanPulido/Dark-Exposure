96c240225be4f89aa05906d490fc7e5f9801614a
Follaabuelas <carlosdanielpulidom2@gmail.com>
2024-11-20 16:29:31 -0600

    Somido

--- Assets/Scripts/Presion Cardiaca.cs
+++ Assets/Scripts/Presion Cardiaca.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static Unity.VisualScripting.Member;
 
+[RequireComponent(typeof(AudioSource))]
 public class PresionCardiaca : MonoBehaviour
 {
     public Latido latido;
@@ -20,32 +22,66 @@ public class PresionCardiaca : MonoBehaviour
     Color pressureColor = Color.white;
     public bool isEnemy = false;
     public bool isSafe = true;
+    public float volumen;
+    public AudioClip[] sounds;
 
     Die died;
 
-    public AudioSource Hearth;
+    AudioSource Hearth;
+
     void Start()
     {
         ActualPressure = MinPressure;
         
+
+        if (Hearth == null)
+        {
+            ObtenerAudioSource();
+        }
     }
 
-   
-    void Update()
+    public void ObtenerAudioSource()
     {
+        Hearth = GetComponent<AudioSource>();
+
+        if (Hearth == null)
+        {
+            Debug.LogError("No se encontró el componente AudioSource en el GameObject.");
+            return;
+        }
+
+        // Configura el AudioSource como sonido 3D
+        Hearth.spatialBlend = 1.0f; // Hace que el sonido sea completamente 3D
+        Hearth.rolloffMode = AudioRolloffMode.Linear; // Cambia esto si deseas un tipo diferente de caída del volumen
+        Hearth.minDistance = 1f; // La distancia mínima a partir de la cual se escuchará el sonido
+        Hearth.maxDistance = 20f; // La distancia máxima a la que el sonido es audible
+    }
+    public void Musica()
+    {
+        int r = UnityEngine.Random.Range(0, sounds.Length);
+        Hearth.PlayOneShot(sounds[r], volumen);
+    }
+
+    public void Update()
+    {
+        if (!Hearth.isPlaying)
+        {
+            Musica();
+        }
 
         ActualPressure = Mathf.Clamp(ActualPressure, MinPressure, MaxPressure);
 
-        if (ActualPressure >= 6)
+        if (ActualPressure >= 250)
         {
             Controller.enabled = false;
             
 
         }
+
         pressureColor.a = ActualPressure;
         PressureBlood.color = pressureColor;
-        Hearth.volume = ActualPressure;
-        Hearth.pitch = ActualPressure;
+        //Hearth.volume = ActualPressure;
+        //Hearth.pitch = ActualPressure;
 
         if (isEnemy) 
         {
